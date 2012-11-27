30d8c29607be9d826e2d3d815092ecf2a38827ce
Ali Can MOGOL <alicanmogol@gmail.com>
2012-11-27 10:28:41 +0200

    first validator implementation, it will be changed to validation POJO

--- src/com/fererlab/validation/ValidationException.java
+++ src/com/fererlab/validation/ValidationException.java
@@ -3,5 +3,9 @@ package com.fererlab.validation;
 /**
  * acm 11/23/12 5:20 PM
  */
-public class ValidationException {
+public class ValidationException extends Exception {
+
+    public ValidationException(String message) {
+        super(message);
+    }
 }
--- src/com/fererlab/validation/Validator.java
+++ src/com/fererlab/validation/Validator.java
@@ -1,7 +1,36 @@
 package com.fererlab.validation;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * acm 11/23/12 5:19 PM
  */
 public class Validator {
+
+    private List<String> validations = new ArrayList<>();
+
+    public Validator() {
+    }
+
+    public Validator(List<String> validations) {
+        this.validations = validations;
+    }
+
+    public void validate() throws ValidationException {
+        throw new ValidationException("Validation Exception");
+    }
+
+    public void addValidation(String validation){
+        validations.add(validation);
+    }
+
+    public void setValidations(List<String> validations) {
+        this.validations = validations;
+    }
+
+    public List<String> getValidations() {
+        return validations;
+    }
 }
