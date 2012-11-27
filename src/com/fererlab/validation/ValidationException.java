539d74a3c895b9392c4e41b4445a440834172667
Ali Can MOGOL <alicanmogol@gmail.com>
2012-11-27 10:27:49 +0200

    JDOM added to project

---
+++ src/com/fererlab/validation/ValidationException.java
@@ -0,0 +1,7 @@
+package com.fererlab.validation;
+
+/**
+ * acm 11/23/12 5:20 PM
+ */
+public class ValidationException {
+}
---
+++ src/com/fererlab/validation/Validator.java
@@ -0,0 +1,7 @@
+package com.fererlab.validation;
+
+/**
+ * acm 11/23/12 5:19 PM
+ */
+public class Validator {
+}
