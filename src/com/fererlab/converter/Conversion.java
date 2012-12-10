be7ce8d6bdfe6b59a9d82300651e83656ed191e7
Ali Can MOGOL <alicanmogol@gmail.com>
2012-12-10 11:59:01 +0200

    flow tests added with some pojos

---
+++ src/com/fererlab/converter/Conversion.java
@@ -0,0 +1,7 @@
+package com.fererlab.converter;
+
+/**
+ * acm 12/3/12 9:44 AM
+ */
+public class Conversion {
+}
---
+++ src/com/fererlab/converter/NoFormatter.java
@@ -0,0 +1,7 @@
+package com.fererlab.converter;
+
+/**
+ * acm 12/3/12 9:41 AM
+ */
+public class NoFormatter implements Formatter {
+}
